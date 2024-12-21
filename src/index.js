c5c9c81d4f8ddc141a1ef53c86f80dd19da42928
Kelvin <kelvinyounglee47@gmail.com>
2024-12-21 20:05:50 +0200

    added eventisterns to home page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import './style.css'
-import {loadHome} from './home.js'
-import{loadMenu} from './menu.js'
-import {loadContact} from './contact.js'
+import './style.css';
+import {loadHome} from './home.js';
+import{loadMenu} from './menu.js';
+import {loadContact} from './contact.js';
+
+//load the homepage 
+loadHome();
+
+const content=document.querySelector('#content');
+const nav=document.querySelector('nav');
+
+nav.addEventListener('click',(e)=>{
+    switch (e.target.textContent) {
+        case 'HOME':
+            loadHome();
+            return;
+        case 'MENU':
+            loadMenu();
+            return;
+        case 'CONTACT':
+            loadContact();
+            return;
+    }
+});
