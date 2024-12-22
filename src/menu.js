24285c4e1ecfad119192024657ed39500b1062c7
Kelvin <kelvinyounglee47@gmail.com>
2024-12-22 20:57:30 +0200

    created menu

---
+++ src/menu.js
@@ -0,0 +1,35 @@
+const menuData={
+    coffee:[
+        {name:'Espresso',price:20.00},
+        {name:'Cappuccino',price:15.00},
+        {name:'Double Espresso',price:16.00},
+        {name:'Latte',price:16.00},
+        {name:'Mocha',price:17.00},
+        {name:'Americano',price:25.00},
+    ],
+    nonCoffee:[
+        {name:'Smoothie',price:10.00},
+        {name:'Hot Chocolate',price:12.00},
+        {name:'Fresh Juice',price:15.00},
+        {name:'Fresh Lemonade',price:14.00},
+        {name:'Milkshake',price:25}
+    ],
+    tea:[
+        {name:'Lemon Tea',price:10.00},
+        {name:'Earl Grey Tea',price:12.00},
+        {name:'Jasmine',price :15.00},
+    ],
+    desserts:[
+        {name:'Cake',price:15.00},
+        {name:'Croissant',price:12.00},
+        {name:'Lemon Pie',price:10.00},
+        {name:'Croissant',price:14.00},
+    ],
+    food:[
+        {name:'Sandwich',price:15.00},
+        {name:'Salad',price:12.00},
+        {name:'Brownies',price:10.00},
+        {name:'Chocolate Muffin',price:14.00},
+    ]
+    
+}
